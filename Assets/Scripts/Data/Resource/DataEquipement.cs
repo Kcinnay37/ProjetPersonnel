38a1bab18b70b6287c947070e72779a4a2a7c8fe
Yannick Gilbert <2130073@collegebart.ca>
2023-03-21 14:38:24 -0400

    initialisation des data pour les monture et les equipement

---
+++ Assets/Scripts/Data/Resource/DataEquipement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DataEquipement : DataResource
+{
+    public struct EquipementBuffs
+    {
+
+    }
+
+    public EquipementBuffs buffs;
+    public Sprite image;
+    public Material playerMaterial;
+}
